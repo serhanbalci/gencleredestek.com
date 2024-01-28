6e20a33763513117bf1fffa884619638c84960e9
Serhan <arghan@gmail.com>
2024-01-28 11:24:20 +0300

    Feature: create company from form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+  get 'companies/create'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'
+
+  resources :companies, only: [:new, :create]
 end
